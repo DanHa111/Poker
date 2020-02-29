34ad87181249d4830707cdd584f81447dba5b97a
dani <daniehaid@tsn.at>
2020-02-29 16:23:47 +0100

    CountCombination

--- Poker.java
+++ Poker.java
@@ -1,5 +1,7 @@
 package poker;
 
+import java.util.ArrayList;
+
 public class Poker {
 
 	public static final int anzahlWerte = 13;
@@ -21,13 +23,79 @@ public class Poker {
 	public static int[] kartenzug(int[] karten)	{
 		int[] zug = new int[anzahlKartenZug];
 		for(int i = 0; i < anzahlKartenZug; i++)	{
-			int index = (int)(Math.random()*(anzahlWerte * anzahlFarben - 1 - i));
+			int index = (int)(Math.random()*(anzahlWerte * anzahlFarben - i));
 			zug[i] = karten[index];
 			int x = karten[index];
-			karten[index] = karten[karten.length - 1];
-			karten[karten.length - 1] = x;
+			karten[index] = karten[karten.length - 1 - i];
+			karten[karten.length - 1 - i] = x;
 		}
 		return zug;
+	}
+	
+	public static void countCombination(int[] zug)	{
+		boolean istStrasse = strasse(zug);
+		boolean hatGleicheFarbe = gleicheFarbe(zug);
+		int paare = 0;
+		boolean drillingExistiert = false;
+		boolean paar = false;
+		for (int i = 0; i < zug.length; i++) {
+			for (int j = 0; j < zug.length; j++) {
+				if(zug[i] % anzahlWerte == zug[j] % anzahlWerte)	{
+					if(!paar)	{
+						paare++;
+						paar = true;
+					} else if(!drillingExistiert)	{
+						drillingExistiert = true;
+						drilling++;
+					} else if(drillingExistiert)	{
+						vierling++;
+					}
+				}
+				paar = false;
+			}
+		}
+		
+		if(paare == 0 && !strasse(zug))	{
+			hoechsteKarte++;
+		} else {
+			einPaar += paare;
+			zweiPaare += paare/2;
+			if(paare == 2 && drillingExistiert)	{
+				fullHouse++;
+			}
+			if(hatGleicheFarbe)	{
+				flush++;
+			}
+			if(istStrasse)	{
+				strasse++;
+				if(hatGleicheFarbe)	{
+					straightFlush++;
+					if(zug[zug.length - 1] % anzahlWerte == anzahlWerte)	{
+						royalFlush++;
+					}
+				}
+			}
+		}
+	}
+	
+	public static boolean strasse(int[] zug)	{
+		int summeWerte = 0;
+		int summeStellen = 0;
+		for (int i = 0; i < zug.length; i++) {
+			summeWerte += (zug[i] % anzahlWerte) - (zug[0] % anzahlWerte);
+		}
+		for (int i = 0; i < zug.length; i++) {
+			summeStellen += i;
+		}
+		return summeWerte == summeStellen;
+	}
+	
+	public static boolean gleicheFarbe(int[] zug)	{
+		int summeFarben = 0;
+		for (int i = 0; i < zug.length; i++) {
+			summeFarben += zug[i] / anzahlWerte;
+		}
+		return summeFarben == (zug[0] / anzahlWerte) * zug.length;
 	}
 	
 	public static void printArray(int[] arr)	{
@@ -38,12 +106,32 @@ public class Poker {
 		System.out.println();
 	}
 	
+	public static void printCombination()	{
+		System.out.println("Höchste Karte: " + hoechsteKarte);
+		System.out.println("Ein Paar: " + einPaar);
+		System.out.println("Zwei Paare: " + zweiPaare);
+		System.out.println("Drilling: " + drilling);
+		System.out.println("Straße: " + strasse);
+		System.out.println("Flush: " + flush);
+		System.out.println("Full House: " + fullHouse);
+		System.out.println("Vierling" + vierling);
+		System.out.println("Straight Flush: " +straightFlush);
+		System.out.println("Royal Flush: " + royalFlush);
+	}
+	
 	public static void main(String[] args) {
 		int[] karten = new int[anzahlWerte * anzahlFarben];
 		for(int i = 0; i < karten.length; i++)	{
 			karten[i] = i;
 		}
-		printArray(kartenzug(karten));
+//		for (int i = 0; i < anzahlVersuche; i++) {
+//			int[] karten2 = karten.clone();
+//			countCombination(kartenzug(karten2));
+//		}
+		int[] zug = kartenzug(karten);
+		printArray(zug);
+		countCombination(zug);
+		printCombination();
 	}
 
 }
